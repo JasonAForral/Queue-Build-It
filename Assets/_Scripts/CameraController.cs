e2da6d1ff981e566c56e18a9e62078b4c6545001
Tsinoyboi <ajsnoface@gmail.com>
2015-05-02 19:01:29 -0700

    one

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    private Transform cameraTilt;
+    private Transform cameraZoom;
+
+    public float rotateSpeed = 1f;
+    public float mouseSlideSpeed = 1f;
+    public float keyboardSlideSpeed = 1f;
+    public float zoomSpeed = 50f;
+    public float panSpeed = 1f;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        //if (null == cameraTilt)
+        cameraTilt = transform.GetChild(0);
+        //if (null == cameraZoom)
+        cameraZoom = cameraTilt.GetChild(0);    
+    }
+	
+	void LateUpdate () {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.Translate(-1.0f * panSpeed * mouseSlideSpeed * new Vector3(Input.GetAxis("Mouse X"), 0.0f, Input.GetAxis("Mouse Y")));
+        }
+	}
+}
